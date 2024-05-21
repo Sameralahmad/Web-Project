1dd7b569ef4bf97c768f6e4315e503fcc66db9f7
Sameralahmad <131540237+Sameralahmad@users.noreply.github.com>
2024-05-22 01:36:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+document.getElementById('contact-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('fooorm').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('prii').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('pricc').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('abouu').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('readmoree').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('freq').scrollIntoView({ behavior: 'smooth' });
+});
